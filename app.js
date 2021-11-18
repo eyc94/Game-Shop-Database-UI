e361f2d8ed6baa8283fcec9727e5eb027d47dabc
Eric Chin <ericychin94@gmail.com>
2021-11-18 03:36:03 -0800

    Added app route that creates table

--- app.js
+++ app.js
@@ -6,8 +6,30 @@ PORT = 20301;                               // Set a port number at the top so i
 // Database connection.
 var db = require('./database/db-connector');
 
-app.get('/', function (req, res) {          // Basic syntax for a route.
-    res.send("The server is running!");     // This function sends the string "The server is running!" to the computer requesting the web site.
+app.get('/', function (req, res) {
+    // Define our queries.
+    query1 = 'DROP TABLE IF EXISTS diagnostic;';
+    query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
+    query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!");';
+    query4 = 'SELECT * FROM diagnostic;';
+
+    // Execute every query in an asynchronous manner, we want each query to finish before the next one starts.
+
+    // DROP TABLE...
+    db.pool.query(query1, function (err, results, fields) {
+        // CREATE TABLE...
+        db.pool.query(query2, function (err, results, fields) {
+            // INSERT INTO...
+            db.pool.query(query3, function (err, results, fields) {
+                // SELECT *...
+                db.pool.query(query4, function (err, results, fields) {
+                    // Send the results to the browser.
+                    let base = "<h1>MySQL Results:</h1>";
+                    res.send(base + JSON.stringify(results));
+                });
+            });
+        });
+    });
 });
 
 
