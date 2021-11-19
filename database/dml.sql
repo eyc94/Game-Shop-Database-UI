dbb6d3e8626fb8e7cd209d0ab3cb9418490579c0
Eric Chin <ericychin94@gmail.com>
2021-11-19 03:30:53 -0800

    Added product dml

--- database/dml.sql
+++ database/dml.sql
@@ -6,13 +6,16 @@
 -- data from back-end programming language.
 
 
--- =====================================
 -- Customers
+-- =====================================
 
 -- Add a new customer.
 INSERT INTO customers (firstName, lastName, email, phoneNumber, address)
 VALUES
 (:firstNameInput, :lastNameInput, :emailInput, :phoneNumberInput, :addressInput);
+
+-- Display all customers.
+SELECT * FROM customers;
 
 -- Search for a customer.
 -- Search by customer ID.
@@ -27,6 +30,12 @@ SELECT * FROM customers WHERE lastName = :lastNameInput;
 -- Search by customer email.
 SELECT * FROM customers WHERE email = :emailInput;
 
+-- Search by customer phone number.
+SELECT * FROM customers WHERE phoneNumber = :phoneNumberInput;
+
+-- Search by customer address.
+SELECT * FROM customers WHERE address = :addressInput;
+
 -- Update a customer.
 -- Update a customer's first name, last name, email, phone number, or address.
 UPDATE customers
@@ -39,5 +48,40 @@ DELETE FROM customers
 WHERE customerID = :customerIDInput;
 
 
--- =====================================
 -- Products
+-- =====================================
+
+-- Add a new product.
+INSERT INTO products (productName, price, publisher, genre)
+VALUES
+(:productNameInput, :priceInput, :publisherInput, :genreInput);
+
+-- Display all products.
+SELECT * FROM products;
+
+-- Search for a product.
+-- Search by product ID.
+SELECT * FROM products WHERE productID = :productIDInput;
+
+-- Search by product name.
+SELECT * FROM products WHERE productName = :productNameInput;
+
+-- Search by price.
+SELECT * FROM products WHERE price = :priceInput;
+
+-- Search by publisher.
+SELECT * FROM products WHERE publisher = :publisherInput;
+
+-- Search by genre.
+SELECT * FROM products WHERE genre = :genreInput;
+
+-- Update a product.
+-- Update a product's name, price, publisher, or genre.
+UPDATE products
+SET productName = :productNameInput, price = :priceInput, publisher = :publisherInput, genre = :genreInput
+WHERE productID = :productIDInput;
+
+-- Delete a product.
+-- We will delete a product by its ID.
+DELETE FROM products
+WHERE productID = :productIDInput;
