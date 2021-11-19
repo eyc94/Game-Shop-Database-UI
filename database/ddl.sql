f2908a33c004777888efa7ad12f60c78487be7c5
Eric Chin <ericychin94@gmail.com>
2021-11-18 19:57:48 -0800

    Changed table names to plural form and created orders table

--- database/ddl.sql
+++ database/ddl.sql
@@ -5,7 +5,7 @@
 -- Data Definition Queries
 
 -- Customer
-CREATE TABLE customer (
+CREATE TABLE customers (
     customerID INT(11) AUTO_INCREMENT NOT NULL,
     firstName VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE customer (
 );
 
 -- Product
-CREATE TABLE product (
+CREATE TABLE products (
     productID INT(11) AUTO_INCREMENT NOT NULL,
     productName VARCHAR(255) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE product (
 );
 
 -- Review
-CREATE TABLE review (
+CREATE TABLE reviews (
     reviewID INT(11) AUTO_INCREMENT NOT NULL,
     customerID INT(11),
     productID INT(11),
@@ -33,14 +33,18 @@ CREATE TABLE review (
     text VARCHAR(255) NOT NULL,
     dateWritten DATE NOT NULL,
     PRIMARY KEY (reviewID),
-    FOREIGN KEY (customerID) REFERENCES customer(customerID) ON DELETE SET NULL ON UPDATE CASCADE,
-    FOREIGN KEY (productID) REFERENCES product(productID) ON DELETE SET NULL ON UPDATE CASCADE
+    FOREIGN KEY (customerID) REFERENCES customers(customerID) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (productID) REFERENCES products(productID) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 -- Order
-CREATE TABLE order (
-
-)
+CREATE TABLE orders (
+    orderID INT(11) AUTO_INCREMENT NOT NULL,
+    customerID INT(11),
+    orderDate DATE,
+    PRIMARY KEY (orderID),
+    FOREIGN KEY (customerID) REFERENCES customers(customerID) ON DELETE SET NULL ON UPDATE CASCADE
+);
 
 
 -- Part (b)
