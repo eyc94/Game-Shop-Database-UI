f2e9a5102c5fa1880124e3836ec9c3c909faac0b
Eric Chin <ericychin94@gmail.com>
2021-11-18 19:35:05 -0800

    Added creating customer table

--- database/ddl.sql
+++ database/ddl.sql
@@ -4,6 +4,16 @@
 -- Part (a)
 -- Data Definition Queries
 
+-- Customer
+CREATE TABLE customer (
+    customerID INT(11) AUTO_INCREMENT NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    PRIMARY KEY (customerID)
+);
+
 
 
 -- Part (b)
