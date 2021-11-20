9d28f08bbf915ce4a2283be34cf92a44ae3b8217
Eric Chin <ericychin94@gmail.com>
2021-11-19 23:58:59 -0800

    Changed handlebars to hbs and changed up engine code

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
-var express = require('express');           // We are using the express library for the web server.
-// Import express-handlebars and default layout uses main.handlebars.
-var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
-var app = express();                        // We need to instantiate an express object to interact with the server in our code.
-PORT = 20301;                               // Set a port number at the top so it's easy to change in the future.
+// We are using the express library for the web server.
+var express = require('express');
+// Import express-handlebars.
+const exphbs = require('express-handlebars');
+// Import body-parser middleware.
+const bodyParser = require('body-parser');
+
+// We need to instantiate an express object to interact with the server in our code.
+var app = express();
+// Set a port number at the top so it's easy to change in the future.
+PORT = 20301;
+
+// Parsing middleware.
+// Parse application/x-www-form-urlendcoded.
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Parse application/json.
+app.use(bodyParser.json());
+
+// Static files.
+app.use(express.static('public'));
+
+// Templating engine.
+const handlebars = exphbs.create({ extname: '.hbs' });
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
 
 // Enable express to handle JSON data as well as form data.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.engine('handlebars', handlebars.engine);    // handlebars.engine handles any files with a .handlebars extension.
-app.set('view engine', 'handlebars');           // Allows us to omit .handlebars extension.
+
 
 // Database connection.
 var db = require('./database/db-connector');
