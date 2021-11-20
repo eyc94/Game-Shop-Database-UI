bb0a7d3ac0b0af8f56954033638a4c8fd8a85105
Eric Chin <ericychin94@gmail.com>
2021-11-19 17:14:50 -0800

    Added middleware to handle json parsing

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var handlebars = require('express-handlebars').create({ defaultLayout: 'main' })
 var app = express();                        // We need to instantiate an express object to interact with the server in our code.
 PORT = 20301;                               // Set a port number at the top so it's easy to change in the future.
 
+// Enable express to handle JSON data as well as form data.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.engine('handlebars', handlebars.engine);    // handlebars.engine handles any files with a .handlebars extension.
 app.set('view engine', 'handlebars');           // Allows us to omit .handlebars extension.
