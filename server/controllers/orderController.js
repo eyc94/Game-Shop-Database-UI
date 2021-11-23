053e82c4cec96c162105e3ede4900523f4e57631
Eric Chin <ericychin94@gmail.com>
2021-11-23 14:49:07 -0800

    Added comments

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,20 +1,24 @@
 // Import the database files to use to connect to db.
-const e = require('express');
 const db = require('../../database/db-connector');
 
+// This handles read route.
 exports.view = (req, res) => {
+    // SELECT query.
     const selectQuery = `SELECT * FROM orders;`;
-
+    // Call query.
     db.pool.query(selectQuery, (error, rows, fields) => {
         const obj = { data: rows };
         res.render('orders', obj);
     });
 };
 
+// This handles the route to create an order.
 exports.create = (req, res) => {
+    // INSERT query.
     const createQuery = `INSERT INTO orders (customerID, orderDate, pickup) VALUES (?, ?, ?);`;
+    // Parameters to pass into the call.
     const inserts = [req.body.customerID, req.body.orderDate, req.body.pickup];
-
+    // Call query.
     db.pool.query(createQuery, inserts, (error, rows, fields) => {
         if (error) {
             console.log(error);
@@ -25,10 +29,13 @@ exports.create = (req, res) => {
     });
 };
 
+// This handles the route to delete an order.
 exports.delete = (req, res) => {
+    // DELETE query.
     const deleteQuery = `DELETE FROM orders WHERE orderID = ?;`;
+    // This is the orderID parameter.
     const inserts = [req.params.id];
-
+    // Call query.
     db.pool.query(deleteQuery, inserts, (error, rows, fields) => {
         if (error) {
             console.log(error);
@@ -41,10 +48,13 @@ exports.delete = (req, res) => {
     });
 };
 
+// This handles the route to lead the user to edit an order.
 exports.edit = (req, res) => {
+    // SELECT query.
     const selectQuery = `SELECT * FROM orders WHERE orderID = ?;`;
+    // ID of the order to update.
     inserts = [req.params.id];
-
+    // Call query.
     db.pool.query(selectQuery, inserts, (error, rows, fields) => {
         if (error) {
             console.log(error);
@@ -56,13 +66,17 @@ exports.edit = (req, res) => {
     });
 };
 
+// This handles the route to update the order after submitting.
 exports.update = (req, res) => {
+    // Destructure form inputs from user.
     const { customerID, orderDate, pickup } = req.body;
+    // UDPATE query.
     const updateQuery = `UPDATE orders
     SET customerID = ?, orderDate = ?, pickup = ?
     WHERE orderID = ?;`;
+    // Parameters for the query.
     const inserts = [customerID, orderDate, pickup, req.params.id];
-
+    // Call query.
     db.pool.query(updateQuery, inserts, (error, rows, fields) => {
         if (error) {
             console.log(error);
