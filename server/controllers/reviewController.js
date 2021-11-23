c502d03eae64c2290454ce7ae46ff767a95b57ea
Eric Chin <ericychin94@gmail.com>
2021-11-23 15:02:13 -0800

    Added comments

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -1,8 +1,11 @@
+// Import database connector files to access database functionality.
 const db = require('../../database/db-connector');
 
+// This handles the read route to view all reviews.
 exports.view = (req, res) => {
+    // SELECT query.
     const selectQuery = `SELECT * FROM reviews;`;
-
+    // Call query.
     db.pool.query(selectQuery, (error, rows, fields) => {
         if (error) {
             console.log(error);
@@ -14,10 +17,13 @@ exports.view = (req, res) => {
     });
 };
 
+// This handles the create route to make new reviews.
 exports.create = (req, res) => {
+    // INSERT query.
     const createQuery = `INSERT INTO reviews (customerID, productID, rating, reviewText, dateWritten) VALUES (?, ?, ?, ?, ?);`;
+    // Parameters for query.
     const inserts = [req.body.customerID, req.body.productID, req.body.rating, req.body.reviewText, req.body.dateWritten];
-
+    // Call query.
     db.pool.query(createQuery, inserts, (error, rows, fields) => {
         if (error) {
             console.log(error);
@@ -28,10 +34,13 @@ exports.create = (req, res) => {
     });
 };
 
+// This handles the delete route to delete an existing review.
 exports.delete = (req, res) => {
+    // DELETE query.
     const deleteQuery = `DELETE FROM reviews WHERE reviewID = ?;`;
+    // Parameters for query.
     const inserts = [req.params.id];
-
+    // Call query.
     db.pool.query(deleteQuery, inserts, (error, rows, fields) => {
         if (error) {
             console.log(error);
@@ -44,10 +53,13 @@ exports.delete = (req, res) => {
     });
 };
 
+// This handles the edit viewing when we want to edit one review.
 exports.edit = (req, res) => {
+    // SELECT query.
     const selectQuery = `SELECT * FROM reviews WHERE reviewID = ?;`;
+    // Parameter for query.
     const inserts = [req.params.id];
-
+    // Call query.
     db.pool.query(selectQuery, inserts, (error, rows, fields) => {
         if (error) {
             console.log(error);
@@ -59,11 +71,15 @@ exports.edit = (req, res) => {
     });
 };
 
+// This handles the update route to update one review entry.
 exports.update = (req, res) => {
+    // Destructure the update form input.
     const { customerID, productID, rating, reviewText, dateWritten } = req.body;
+    // UPDATE query.
     const updateQuery = `UPDATE reviews SET customerID = ?, productID = ?, rating = ?, reviewText = ?, dateWritten = ? WHERE reviewID = ?`;
+    // Parameters for query.
     const inserts = [customerID, productID, rating, reviewText, dateWritten, req.params.id];
-
+    // Call query.
     db.pool.query(updateQuery, inserts, (error, rows, fields) => {
         if (error) {
             console.log(error);
