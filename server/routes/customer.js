ffc400fbbcf55dee8c3275c8f8f0c0923d6afe2c
Eric Chin <ericychin94@gmail.com>
2021-11-22 21:36:40 -0800

    Added comments and cleaned up customer route file

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -1,20 +1,22 @@
+// Import the express module.
 const express = require('express');
+// Create the router object from the Router() function.
 const router = express.Router();
+// Import the controller/logic of the route.
 const customerController = require('../controllers/customerController');
 
-// Create, Read, Update, Delete.
+// View customers.
 router.get('/', customerController.view);
 
+// Delete a customer.
 router.get('/:id', customerController.delete);
-
 
 // Create a customer.
 router.post('/addCustomer', customerController.create);
 
-
-
-
+// Edit and Update a customer.
 router.get('/editCustomer/:id', customerController.edit);
 router.post('/editCustomer/:id', customerController.update);
 
+// Export the router object to use outside of this module.
 module.exports = router;
