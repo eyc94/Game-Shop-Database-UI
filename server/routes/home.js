2aa14661f30ee452e1ffd3f9b14512c21809b858
Eric Chin <ericychin94@gmail.com>
2021-11-22 21:46:04 -0800

    Added comments

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,7 +1,12 @@
+// Import express module.
 const express = require('express');
+// Create router object from Router() function.
 const router = express.Router();
+// Import the controller/logic for the home route.
 const homeController = require('../controllers/homeController');
 
+// GET route for the home page.
 router.get('/', homeController.view);
 
+// Export the object to use in app.js.
 module.exports = router;
