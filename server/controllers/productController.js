37cd75321d75ee2e525f0e719fef5c48a646343e
Eric Chin <ericychin94@gmail.com>
2021-11-23 02:44:25 -0800

    Added comments

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,19 +1,24 @@
-const e = require('express');
+// Import the db connection module to run queries.
 const db = require('../../database/db-connector');
 
+// This is the VIEW/READ functionality on the products table.
 exports.view = (req, res) => {
+    // SELECT query.
     let selectQuery = `SELECT * FROM products;`;
-
+    // Call query.
     db.pool.query(selectQuery, (errors, rows, fields) => {
         const obj = { data: rows };
         res.render('products', obj);
     })
 };
 
+// This is the CREATE functionality on the products table.
 exports.create = (req, res) => {
+    // INSERT query.
     let createQuery = `INSERT INTO products (productName, price, publisher, genre) VALUES (?, ?, ?, ?);`;
+    // List of parameters for the ?s.
     let inserts = [req.body.productName, req.body.price, req.body.publisher, req.body.genre];
-
+    // Call query.
     db.pool.query(createQuery, inserts, (error, rows, fields) => {
         if (error) {
             console.log(error);
@@ -24,10 +29,13 @@ exports.create = (req, res) => {
     });
 };
 
+// This is the DELETE functionality on the products table.
 exports.delete = (req, res) => {
+    // DELETE query.
     let deleteQuery = `DELETE FROM products WHERE productID = ?;`;
+    // Parameters for the ?s.
     let inserts = [req.params.id];
-
+    // Call query.
     db.pool.query(deleteQuery, inserts, (error, rows, fields) => {
         if (error) {
             console.log(error);
@@ -40,10 +48,14 @@ exports.delete = (req, res) => {
     });
 };
 
+// This is the EDIT functionality on the products table.
 exports.edit = (req, res) => {
+    // SELECT query.
     let selectQuery = `SELECT * FROM products WHERE productID = ?;`;
+    // Parameters for the ?s.
     let inserts = [req.params.id];
-
+    // Call query.
+    // Notice that we are redirecting users to a new page to edit values.
     db.pool.query(selectQuery, inserts, (error, rows, fields) => {
         if (error) {
             console.log(error);
@@ -55,13 +67,17 @@ exports.edit = (req, res) => {
     });
 };
 
+// This is the UPDATE functionality on the products table.
 exports.update = (req, res) => {
+    // Destructure the information we get from updating our table (new values).
     const { productName, price, publisher, genre } = req.body;
+    // UPDATE query.
     let updateQuery = `UPDATE products
     SET productName = ?, price = ?, publisher = ?, genre = ?
     WHERE productID = ?;`;
+    // List of parameters for the ?s.
     let inserts = [productName, price, publisher, genre, req.params.id];
-
+    // Call query.
     db.pool.query(updateQuery, inserts, (error, rows, fields) => {
         if (error) {
             console.log(error);
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,13 +1,20 @@
+// Import express module.
 const express = require('express');
+// Create router object from express module.
 const router = express.Router();
+// Import the controller/logic code for the routes.
 const productController = require('../controllers/productController');
 
-router.get('/', productController.view);
-
+// Create.
 router.post('/addProduct', productController.create);
 
+// Read.
+router.get('/', productController.view);
+
+// Delete.
 router.get('/:id', productController.delete);
 
+// Edit and Update.
 router.get('/editProduct/:id', productController.edit);
 router.post('/editProduct/:id', productController.update);
 
