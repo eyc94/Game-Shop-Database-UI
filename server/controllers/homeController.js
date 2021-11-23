10af3027502646a7c7675f1c84d7e811f45a75dc
Eric Chin <ericychin94@gmail.com>
2021-11-22 21:44:54 -0800

    Added comments

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,7 +1,8 @@
+// Import database connection module in case we need to connect to module.
 const db = require('../../database/db-connector');
 
 // View Customers. 
 exports.view = (req, res) => {
+    // Render the home.hbs handlebars file.
     res.render('home');
-
 };
